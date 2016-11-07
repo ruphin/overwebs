dcd870eb0aab6033644aa737af792abb66403b41
Goffert van Gool <goffert@phusion.nl>
2016-11-07 01:26:39 +0100

    Add season2 banner info

--- app/elements/overwebs-play-page/overwebs-play-page.js
+++ app/elements/overwebs-play-page/overwebs-play-page.js
@@ -7,8 +7,20 @@ Polymer({
   },
   ready: function() {
     this.$.backButton.onclick = (event) => {
+      this.$.season2.classList.remove('visible');
+      this.$.season2.querySelector('.banner').classList.remove('visible');
       event.stopPropagation();
       history.back();
-    }
+    };
+    this.$.overlayButton.onclick = (event) => {
+      this.$.season2.classList.remove('visible');
+      this.$.season2.querySelector('.banner').classList.remove('visible');
+      event.stopPropagation();
+      history.back();
+    };
+    this.$.seasonInfoButton.onclick = (event) => {
+      this.$.season2.classList.add('visible');
+      setTimeout(() => { this.$.season2.querySelector('.banner').classList.add('visible')}, 0);
+    };
   }
 });
