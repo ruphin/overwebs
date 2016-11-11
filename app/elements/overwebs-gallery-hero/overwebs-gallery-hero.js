7df0829fb663eb4234c63e7292dd96a350d4b60d
Goffert van Gool <goffert@phusion.nl>
2016-11-11 01:07:03 +0100

    Add gallery-hero glow on 'new' label

--- app/elements/overwebs-gallery-hero/overwebs-gallery-hero.js
+++ app/elements/overwebs-gallery-hero/overwebs-gallery-hero.js
@@ -33,11 +33,16 @@ Polymer({
     '_heroChanged(hero, unlocked)'
   ],
 
+  ready: function() {
+    this.$.glow.addEventListener('animationiteration', (e) => e.target.style = `transform: translate(-50%,-50%) rotate(${Math.floor(Math.random() * 360)}deg);`);
+  },
+
   _heroChanged: function(hero, unlocked) {
     this.classList.add(hero.id)
     this.$.name.style.fontSize = ((hero.scaling || 1) * 2.0833) + 'vw'
     this.$.completion.style.background = hero.color || 'white';
     this.$.completion.style.width = (unlocked / (hero.unlockable || 1) * 6.5625) + "vw";
+
   },
 
   _color: function(hero) {
