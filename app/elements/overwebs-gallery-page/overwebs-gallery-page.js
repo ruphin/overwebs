a8d1c89d2ae249bb3c1383b1bcb51d039de34429
Goffert van Gool <goffert@phusion.nl>
2016-11-14 22:47:13 +0100

    I can haxx

--- app/elements/overwebs-gallery-page/overwebs-gallery-page.js
+++ app/elements/overwebs-gallery-page/overwebs-gallery-page.js
@@ -1,13 +1,47 @@
 Polymer({
   is: 'overwebs-gallery-page',
+  properties: {
+    heroData: {
+      type: Object,
+      notify: true,
+    },
+    playerData: {
+      type: Object,
+      notify: true,
+    }
+  },
 
   ready: function() {
     this.$.backButton.onclick = (event) => {
       event.stopPropagation();
       history.back();
     }
+    this.$.haxxOn.onclick = (event) => {
+      for (hero in this.heroData) {
+        this.playerData.unlocks[hero] = this.heroData[hero].unlockable;
+        this.heroData[hero].background = true;
+      }
+
+      let tmpHeroData = this.heroData;
+      this.set('heroData', {});
+      setTimeout(_ => {
+        this.set('heroData', tmpHeroData);
+      }, 0)
+    }
+    this.$.haxxOff.onclick = (event) => {
+      for (hero in this.heroData) {
+        this.playerData.unlocks[hero] = 0;
+        this.heroData[hero].background = false;
+      }
+
+      let tmpHeroData = this.heroData;
+      this.set('heroData', {});
+      setTimeout(_ => {
+        this.set('heroData', tmpHeroData)
+      }, 0)
+    }
   },
-  
+
   _heroes: function(heroData) {
     return Object.keys(heroData).map(key => heroData[key])
   },
