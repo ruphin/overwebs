0f86609fae3259450f7e25646c86420a4cc2261e
Goffert van Gool <goffert@phusion.nl>
2016-12-12 00:38:40 +0100

    Add loading spinner element

--- app/elements/overwebs-loading-spinner/overwebs-loading-spinner.js
+++ app/elements/overwebs-loading-spinner/overwebs-loading-spinner.js
@@ -1,3 +1,15 @@
 Polymer({
   is: 'overwebs-loading-spinner',
+  properties: {
+    size: {
+      type: Number,
+      value: 52,
+      observer: '_sizeChanged'
+    },
+  },
+
+  _sizeChanged: function() {
+    this.$.svg.style.width = `calc(${this.size} / 25.6 * 1vw)`;
+    this.$.svg.style.height = `calc(${this.size} / 25.6 * 1vw)`;
+  }
 });
